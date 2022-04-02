3081d520c64cc6a92b67c8d13b756a39d93a1ed8
Jack Callaghan <callaghanj1@udayton.edu>
2022-04-02 01:54:55 -0400

    Created new form for world creation

---
+++ Server_tab/NewWorldForm.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace MinecraftServerCSharp.Server_tab
+{
+    partial class NewWorldForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newWorldGroupbox = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel6 = new System.Windows.Forms.FlowLayoutPanel();
+            this.createWorldButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel5 = new System.Windows.Forms.FlowLayoutPanel();
+            this.verifyButton = new System.Windows.Forms.Button();
+            this.verifyLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.difficultyLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.seedLabel = new System.Windows.Forms.Label();
+            this.seedTextbox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextbox = new System.Windows.Forms.TextBox();
+            this.resourcepackTextbox = new System.Windows.Forms.TextBox();
+            this.newWorldGroupbox.SuspendLayout();
+            this.flowLayoutPanel6.SuspendLayout();
+            this.flowLayoutPanel5.SuspendLayout();
+            this.flowLayoutPanel4.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // newWorldGroupbox
+            // 
+            this.newWorldGroupbox.Controls.Add(this.flowLayoutPanel6);
+            this.newWorldGroupbox.Controls.Add(this.flowLayoutPanel5);
+            this.newWorldGroupbox.Controls.Add(this.flowLayoutPanel4);
+            this.newWorldGroupbox.Controls.Add(this.flowLayoutPanel3);
+            this.newWorldGroupbox.Controls.Add(this.flowLayoutPanel2);
+            this.newWorldGroupbox.Controls.Add(this.flowLayoutPanel1);
+            this.newWorldGroupbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.newWorldGroupbox.Location = new System.Drawing.Point(0, 0);
+            this.newWorldGroupbox.Name = "newWorldGroupbox";
+            this.newWorldGroupbox.Size = new System.Drawing.Size(334, 286);
+            this.newWorldGroupbox.TabIndex = 0;
+            this.newWorldGroupbox.TabStop = false;
+            this.newWorldGroupbox.Text = "````";
+            // 
+            // flowLayoutPanel6
+            // 
+            this.flowLayoutPanel6.Controls.Add(this.createWorldButton);
+            this.flowLayoutPanel6.Controls.Add(this.cancelButton);
+            this.flowLayoutPanel6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel6.Location = new System.Drawing.Point(3, 253);
+            this.flowLayoutPanel6.MaximumSize = new System.Drawing.Size(0, 30);
+            this.flowLayoutPanel6.MinimumSize = new System.Drawing.Size(0, 30);
+            this.flowLayoutPanel6.Name = "flowLayoutPanel6";
+            this.flowLayoutPanel6.Size = new System.Drawing.Size(328, 30);
+            this.flowLayoutPanel6.TabIndex = 6;
+            // 
+            // createWorldButton
+            // 
+            this.createWorldButton.AutoSize = true;
+            this.createWorldButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.createWorldButton.Location = new System.Drawing.Point(3, 3);
+            this.createWorldButton.MaximumSize = new System.Drawing.Size(130, 25);
+            this.createWorldButton.MinimumSize = new System.Drawing.Size(130, 25);
+            this.createWorldButton.Name = "createWorldButton";
+            this.createWorldButton.Size = new System.Drawing.Size(130, 25);
+            this.createWorldButton.TabIndex = 5;
+            this.createWorldButton.Text = "Create New World";
+            this.createWorldButton.UseVisualStyleBackColor = true;
+            this.createWorldButton.Click += new System.EventHandler(this.createWorldButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.cancelButton.Location = new System.Drawing.Point(191, 3);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(55, 3, 3, 3);
+            this.cancelButton.MaximumSize = new System.Drawing.Size(130, 25);
+            this.cancelButton.MinimumSize = new System.Drawing.Size(130, 25);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(130, 25);
+            this.cancelButton.TabIndex = 6;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel5
+            // 
+            this.flowLayoutPanel5.Controls.Add(this.verifyButton);
+            this.flowLayoutPanel5.Controls.Add(this.verifyLabel);
+            this.flowLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel5.Location = new System.Drawing.Point(3, 149);
+            this.flowLayoutPanel5.MaximumSize = new System.Drawing.Size(0, 30);
+            this.flowLayoutPanel5.MinimumSize = new System.Drawing.Size(0, 30);
+            this.flowLayoutPanel5.Name = "flowLayoutPanel5";
+            this.flowLayoutPanel5.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.flowLayoutPanel5.Size = new System.Drawing.Size(328, 30);
+            this.flowLayoutPanel5.TabIndex = 4;
+            // 
+            // verifyButton
+            // 
+            this.verifyButton.Location = new System.Drawing.Point(3, 3);
+            this.verifyButton.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.verifyButton.Name = "verifyButton";
+            this.verifyButton.Size = new System.Drawing.Size(110, 25);
+            this.verifyButton.TabIndex = 0;
+            this.verifyButton.Text = "Verify URL";
+            this.verifyButton.UseVisualStyleBackColor = true;
+            this.verifyButton.Click += new System.EventHandler(this.verifyButton_Click);
+            // 
+            // verifyLabel
+            // 
+            this.verifyLabel.AutoSize = true;
+            this.verifyLabel.Location = new System.Drawing.Point(119, 3);
+            this.verifyLabel.MaximumSize = new System.Drawing.Size(0, 25);
+            this.verifyLabel.MinimumSize = new System.Drawing.Size(200, 25);
+            this.verifyLabel.Name = "verifyLabel";
+            this.verifyLabel.Size = new System.Drawing.Size(200, 25);
+            this.verifyLabel.TabIndex = 1;
+            this.verifyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.Controls.Add(this.label1);
+            this.flowLayoutPanel4.Controls.Add(this.resourcepackTextbox);
+            this.flowLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(3, 119);
+            this.flowLayoutPanel4.MaximumSize = new System.Drawing.Size(0, 30);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(328, 30);
+            this.flowLayoutPanel4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.MaximumSize = new System.Drawing.Size(110, 25);
+            this.label1.MinimumSize = new System.Drawing.Size(110, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resource Pack URL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.difficultyLabel);
+            this.flowLayoutPanel3.Controls.Add(this.comboBox1);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 79);
+            this.flowLayoutPanel3.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+            this.flowLayoutPanel3.MaximumSize = new System.Drawing.Size(0, 40);
+            this.flowLayoutPanel3.MinimumSize = new System.Drawing.Size(0, 40);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(328, 40);
+            this.flowLayoutPanel3.TabIndex = 2;
+            // 
+            // difficultyLabel
+            // 
+            this.difficultyLabel.AutoSize = true;
+            this.difficultyLabel.Location = new System.Drawing.Point(3, 3);
+            this.difficultyLabel.MaximumSize = new System.Drawing.Size(110, 25);
+            this.difficultyLabel.MinimumSize = new System.Drawing.Size(110, 25);
+            this.difficultyLabel.Name = "difficultyLabel";
+            this.difficultyLabel.Size = new System.Drawing.Size(110, 25);
+            this.difficultyLabel.TabIndex = 0;
+            this.difficultyLabel.Text = "Difficulty";
+            this.difficultyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(119, 6);
+            this.comboBox1.MaximumSize = new System.Drawing.Size(200, 0);
+            this.comboBox1.MinimumSize = new System.Drawing.Size(200, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 23);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.seedLabel);
+            this.flowLayoutPanel2.Controls.Add(this.seedTextbox);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 49);
+            this.flowLayoutPanel2.MaximumSize = new System.Drawing.Size(0, 30);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(328, 30);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // seedLabel
+            // 
+            this.seedLabel.AutoSize = true;
+            this.seedLabel.Location = new System.Drawing.Point(3, 3);
+            this.seedLabel.MaximumSize = new System.Drawing.Size(110, 25);
+            this.seedLabel.MinimumSize = new System.Drawing.Size(110, 25);
+            this.seedLabel.Name = "seedLabel";
+            this.seedLabel.Size = new System.Drawing.Size(110, 25);
+            this.seedLabel.TabIndex = 0;
+            this.seedLabel.Text = "Seed";
+            this.seedLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // seedTextbox
+            // 
+            this.seedTextbox.Location = new System.Drawing.Point(119, 6);
+            this.seedTextbox.MaximumSize = new System.Drawing.Size(200, 20);
+            this.seedTextbox.MinimumSize = new System.Drawing.Size(200, 20);
+            this.seedTextbox.Name = "seedTextbox";
+            this.seedTextbox.Size = new System.Drawing.Size(200, 20);
+            this.seedTextbox.TabIndex = 1;
+            this.seedTextbox.TextChanged += new System.EventHandler(this.seedTextbox_TextChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.nameLabel);
+            this.flowLayoutPanel1.Controls.Add(this.nameTextbox);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 19);
+            this.flowLayoutPanel1.MaximumSize = new System.Drawing.Size(0, 30);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 3, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(328, 30);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(3, 3);
+            this.nameLabel.MaximumSize = new System.Drawing.Size(110, 25);
+            this.nameLabel.MinimumSize = new System.Drawing.Size(110, 25);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(110, 25);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "World Name";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // nameTextbox
+            // 
+            this.nameTextbox.Location = new System.Drawing.Point(119, 6);
+            this.nameTextbox.MaximumSize = new System.Drawing.Size(200, 20);
+            this.nameTextbox.MinimumSize = new System.Drawing.Size(200, 20);
+            this.nameTextbox.Name = "nameTextbox";
+            this.nameTextbox.Size = new System.Drawing.Size(200, 20);
+            this.nameTextbox.TabIndex = 1;
+            this.nameTextbox.TextChanged += new System.EventHandler(this.nameTextbox_TextChanged);
+            // 
+            // resourcepackTextbox
+            // 
+            this.resourcepackTextbox.Location = new System.Drawing.Point(119, 6);
+            this.resourcepackTextbox.MaximumSize = new System.Drawing.Size(200, 20);
+            this.resourcepackTextbox.MinimumSize = new System.Drawing.Size(200, 20);
+            this.resourcepackTextbox.Name = "resourcepackTextbox";
+            this.resourcepackTextbox.Size = new System.Drawing.Size(200, 20);
+            this.resourcepackTextbox.TabIndex = 2;
+            this.resourcepackTextbox.TextChanged += new System.EventHandler(this.resourcepackTextbox_TextChanged);
+            // 
+            // NewWorldForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(334, 286);
+            this.Controls.Add(this.newWorldGroupbox);
+            this.MaximumSize = new System.Drawing.Size(350, 325);
+            this.MinimumSize = new System.Drawing.Size(350, 325);
+            this.Name = "NewWorldForm";
+            this.Text = "NewWorldForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NewWorldForm_FormClosing);
+            this.newWorldGroupbox.ResumeLayout(false);
+            this.flowLayoutPanel6.ResumeLayout(false);
+            this.flowLayoutPanel6.PerformLayout();
+            this.flowLayoutPanel5.ResumeLayout(false);
+            this.flowLayoutPanel5.PerformLayout();
+            this.flowLayoutPanel4.ResumeLayout(false);
+            this.flowLayoutPanel4.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox newWorldGroupbox;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private Label seedLabel;
+        private TextBox seedTextbox;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label nameLabel;
+        private TextBox nameTextbox;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private Label difficultyLabel;
+        private Label seperator;
+        private FlowLayoutPanel flowLayoutPanel5;
+        private Button verifyButton;
+        private FlowLayoutPanel flowLayoutPanel4;
+        private Label label1;
+        private TextBox resourcepackTextbox;
+        private ComboBox comboBox1;
+        private Label verifyLabel;
+        private FlowLayoutPanel flowLayoutPanel6;
+        private Button createWorldButton;
+        private Button cancelButton;
+        private TextBox textBox1;
+    }
+}
---
+++ Server_tab/NewWorldForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace MinecraftServerCSharp.Server_tab
+{
+    public partial class NewWorldForm : Form
+    {
+        public string[] cfg;
+        private string sp_path;
+        private string[] world_names;
+        private int difficulty;
+        private int seed;
+        private int name;
+        private int rp;
+        private int rp_enable;
+        public NewWorldForm(String pd, string[] worlds)
+        {
+            this.world_names = worlds;
+            this.sp_path = pd + "\\Server\\server.properties";
+            InitializeComponent();
+            cfg = readCFG(sp_path);
+        }
+        public void NewWorldForm_FormClosing(object sender, EventArgs args)
+        {
+            return;
+        }
+        //
+        // textboxes
+        //
+        private void nameTextbox_TextChanged(object sender, EventArgs e)
+        {
+            cfg[name] = "level-name=worlds/" + nameTextbox.Text;
+        }
+        private void seedTextbox_TextChanged(object sender, EventArgs e)
+        {
+            cfg[seed] = "level-seed=" + seedTextbox.Text;
+        }
+        private void resourcepackTextbox_TextChanged(object sender, EventArgs e)
+        {
+            if (this.resourcepackTextbox.Text == "")
+            {
+                cfg[rp_enable] = "require-resource-pack=false";
+            }
+            else
+            {
+                if (verifyResourcepack(resourcepackTextbox.Text))
+                {
+                    cfg[rp_enable] = "require-resource-pack=true";
+                    string resource_pack = "resource-pack=" + resourcepackTextbox.Text;
+                    cfg[rp] = resource_pack;
+                }
+                else 
+                {
+                    cfg[rp_enable] = "require-resource-pack=false";
+                    cfg[rp] = "resource-pack=";
+                }
+
+            }
+        }
+        //
+        // Resource pack verify
+        //
+        private bool verifyResourcepack(string url)
+        {
+            try
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = "HEAD";
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                bool return_val = response.StatusCode == HttpStatusCode.OK;
+                response.Close();
+                return return_val;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        private void verifyButton_Click(object sender, EventArgs e)
+        {
+            if (verifyResourcepack(this.resourcepackTextbox.Text))
+            {
+                this.verifyLabel.Text = "URL is verified!";
+            }
+            else
+                this.verifyLabel.Text = "This URL is invalid!";
+        }
+        //
+        // Combobox
+        //
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //
+        // CFG methods
+        //
+        private string[] readCFG(string path)
+        {
+            string[] allLines = File.ReadAllLines(path); int i = 0;
+            resetCFG(allLines);
+            return allLines;
+        }
+        private void resetCFG(string[] cfg)
+        {
+            int i = 0;
+            foreach (string line in cfg)
+            {
+                if (line.StartsWith("level-seed"))
+                {
+                    string[] l = line.Split('=');
+                    cfg[i] = l[0] + "=";
+                    seed = i;
+                }
+                else if (line.StartsWith("resource-pack="))
+                {
+                    string[] l = line.Split("=");
+                    cfg[i] = l[0] + "=";
+                    rp = i;
+                }
+                else if (line.StartsWith("level-name"))
+                {
+                    string[] l = line.Split('=');
+                    cfg[i] = l[0] + "=worlds/";
+                    name = i;
+                }
+                else if (line.StartsWith("require-resource-pack="))
+                {
+                    string[] l = line.Split('=');
+                    cfg[i] = l[0] + "=false";
+                    rp_enable = i;
+                }
+                else if (line.StartsWith("difficulty"))
+                {
+
+                    string[] l = line.Split('=');
+                    cfg[i] = l[0] + "=hard";
+                    difficulty = i;
+                }
+                i++;
+            }
+        }
+
+        private void createWorldButton_Click(object sender, EventArgs e)
+        {
+            if (this.nameTextbox.Text == "")
+            {
+                return;
+            }
+            //else if (this.nameTextbox.Text is in )
+            File.WriteAllLines(sp_path, cfg);
+            this.Close();
+        }
+
+    }
+}
